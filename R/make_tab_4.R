9798e65f25839c8f465a21e701eb7172c3c4b0cf
PaulESantos <paulefrens@gmail.com>
2024-02-23 23:55:24 -0500

    tab 4

--- R/make_tab_4.R
+++ R/make_tab_4.R
@@ -13,55 +13,56 @@
 #' \dontrun{
 #' df <-  get_tab_4("rawdata/08TOMO_01.xlsx", sheet = 4)
 #' }
- get_tab_4<- function(file, sheet, dep_name = NULL) {
-  df <- readxl::read_excel(file, sheet = sheet, col_names = FALSE) |>
-    dplyr::select(-2) |>
-    purrr::set_names(c("parentesco", "Menores de 1 ano", "1 a 14", "15 a 29",
-                       "30 a 44", "45 a 64", "65 y mas")) |>
-    janitor::make_clean_names() |>
-    dplyr::filter(!is.na(parentesco)) |>
-    dplyr::filter(!stringr::str_detect(parentesco, "^Fuente")) |>
-    dplyr::mutate(distrito = dplyr::if_else(stringr::str_detect(parentesco, "^DISTRITO"),
-                                            parentesco,
-                                            NA_character_)) |>
-    dplyr::mutate(provincia = dplyr::if_else(stringr::str_detect(parentesco, "^PROVINCIA"),
+get_tab_4 <- function(file, sheet, dep_name = NULL) {
+   readxl::read_excel(file,
+                      sheet = sheet,
+                      col_names = FALSE,
+                      skip = 4) |>
+     dplyr::select(-2) |>
+     purrr::set_names(c("parentesco", "Menores de 1 ano", "1 a 14", "15 a 29",
+                        "30 a 44", "45 a 64", "65 y mas") |>
+                        janitor::make_clean_names() ) |>
+     dplyr::filter(!is.na(parentesco)) |>
+     dplyr::filter(!stringr::str_detect(parentesco, "^Fuente")) |>
+     dplyr::mutate(distrito = dplyr::if_else(stringr::str_detect(parentesco, "^DISTRITO"),
                                              parentesco,
                                              NA_character_)) |>
-    tidyr::fill(provincia, .direction = "down") |>
-    tidyr::fill(distrito, .direction = "down") |>
-    dplyr::mutate(distribucion = dplyr::case_when(
-      stringr::str_detect(parentesco, "^URBANA") ~ parentesco,
-      stringr::str_detect(parentesco, "^RURAL") ~ parentesco,
-      stringr::str_detect(parentesco, "^DISTRITO") ~ parentesco,
-      TRUE ~ NA_character_
-    )) |>
-    tidyr::fill(distribucion, .direction = "down") |>
-    dplyr::mutate(sexo = dplyr::case_when(
-      stringr::str_detect(parentesco, "^Hombres") ~ parentesco,
-      stringr::str_detect(parentesco, "^Mujeres") ~ parentesco,
-      stringr::str_detect(parentesco, "^URBANA") ~ parentesco,
-      stringr::str_detect(parentesco, "^RURAL") ~ parentesco,
-      TRUE ~ NA_character_
-    )) |>
-    tidyr::fill(sexo, .direction = "down") |>
-    dplyr::filter(stringr::str_detect(sexo, "^Hombres|^Mujeres")) |>
-    dplyr::filter(!is.na(distrito)) |>
-    dplyr::filter(parentesco != distrito) |>
-    dplyr::filter(parentesco != sexo) |>
-    dplyr::filter(distribucion %in% c("URBANA", "RURAL")) |>
-    dplyr::mutate(provincia = stringr::str_remove(provincia, "PROVINCIA "),
-                  distrito = stringr::str_remove(distrito, "^DISTRITO ")) |>
-    tidyr::pivot_longer(-c(provincia, distrito, distribucion, sexo, parentesco),
-                        names_to = "rango_etareo",
-                        values_to = "poblacion") |>
-    dplyr::mutate(poblacion = stringr::str_replace(poblacion,
-                                                   "-",
-                                                   NA_character_)) |>
-    dplyr::mutate(rango_etareo = stringr::str_remove(rango_etareo,
-                                                     "^x"))
-
-  return(df |>
-           dplyr::mutate(departamento = {{dep_name}}) |>
-           dplyr::relocate(departamento))
+     dplyr::mutate(provincia = dplyr::if_else(stringr::str_detect(parentesco, "^PROVINCIA"),
+                                              parentesco,
+                                              NA_character_)) |>
+     tidyr::fill(provincia, .direction = "down") |>
+     tidyr::fill(distrito, .direction = "down") |>
+     dplyr::mutate(distribucion = dplyr::case_when(
+       stringr::str_detect(parentesco, "^URBANA") ~ parentesco,
+       stringr::str_detect(parentesco, "^RURAL") ~ parentesco,
+       stringr::str_detect(parentesco, "^DISTRITO") ~ parentesco,
+       TRUE ~ NA_character_
+     )) |>
+     tidyr::fill(distribucion, .direction = "down") |>
+     dplyr::mutate(sexo = dplyr::case_when(
+       stringr::str_detect(parentesco, "^Hombres") ~ parentesco,
+       stringr::str_detect(parentesco, "^Mujeres") ~ parentesco,
+       stringr::str_detect(parentesco, "^URBANA") ~ parentesco,
+       stringr::str_detect(parentesco, "^RURAL") ~ parentesco,
+       TRUE ~ NA_character_
+     )) |>
+     tidyr::fill(sexo, .direction = "down") |>
+     dplyr::filter(stringr::str_detect(sexo, "^Hombres|^Mujeres")) |>
+     dplyr::filter(!is.na(distrito)) |>
+     dplyr::filter(parentesco != distrito) |>
+     dplyr::filter(parentesco != sexo) |>
+     dplyr::filter(distribucion %in% c("URBANA", "RURAL")) |>
+     dplyr::mutate(provincia = stringr::str_remove(provincia, "PROVINCIA "),
+                   distrito = stringr::str_remove(distrito, "^DISTRITO ")) |>
+     tidyr::pivot_longer(-c(provincia, distrito, distribucion, sexo, parentesco),
+                         names_to = "rango_etareo",
+                         values_to = "poblacion") |>
+     dplyr::mutate(poblacion = stringr::str_replace(poblacion,
+                                                    "-",
+                                                    NA_character_)) |>
+     dplyr::mutate(rango_etareo = stringr::str_remove(rango_etareo,
+                                                      "^x")) |>
+     dplyr::mutate(departamento = {{dep_name}}) |>
+     dplyr::relocate(departamento)
 }
 
